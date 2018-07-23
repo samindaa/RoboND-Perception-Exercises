181e06acab898178cbbcb4b69e857984697c6092
robond <samindaa@gmail.com>
2018-07-22 19:10:52 -0700

    updates

--- Exercise-2/sensor_stick/scripts/segmentation.py
+++ Exercise-2/sensor_stick/scripts/segmentation.py
@@ -8,11 +8,11 @@ from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
 from pcl_helper import *
 
+
 # TODO: Define functions as required
 
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
-
     # TODO: Convert ROS msg to PCL data
     cloud = ros_to_pcl(pcl_msg)
 
@@ -31,8 +31,8 @@ def pcl_callback(pcl_msg):
 
     # Call the filter function to obtain the resultant downsampled point cloud
     cloud_filtered = vox.filter()
-    #filename = 'voxel_downsampled.pcd'
-    #pcl.save(cloud_filtered, filename)
+    # filename = 'voxel_downsampled.pcd'
+    # pcl.save(cloud_filtered, filename)
 
     # TODO: PassThrough Filter
     # Create a PassThrough filter object.
@@ -47,8 +47,8 @@ def pcl_callback(pcl_msg):
 
     # Finally use the filter function to obtain the resultant point cloud.
     cloud_filtered = passthrough.filter()
-    #filename = 'pass_through_filtered.pcd'
-    #pcl.save(cloud_filtered, filename)
+    # filename = 'pass_through_filtered.pcd'
+    # pcl.save(cloud_filtered, filename)
 
     # TODO: RANSAC Plane Segmentation
     # Create the segmentation object
@@ -70,12 +70,12 @@ def pcl_callback(pcl_msg):
     # TODO: Extract inliers and outliers
     # Extract inliers
     extracted_inliers = cloud_filtered.extract(inliers, negative=False)
-    #filename = 'extracted_inliers.pcd'
-    #pcl.save(extracted_inliers, filename)
+    # filename = 'extracted_inliers.pcd'
+    # pcl.save(extracted_inliers, filename)
 
     extracted_outliers = cloud_filtered.extract(inliers, negative=True)
-    #filename = 'extracted_outliers.pcd'
-    #pcl.save(extracted_outliers, filename)
+    # filename = 'extracted_outliers.pcd'
+    # pcl.save(extracted_outliers, filename)
 
     ros_cloud_table = pcl_to_ros(extracted_inliers)
     ros_cloud_objects = pcl_to_ros(extracted_outliers)
@@ -84,22 +84,46 @@ def pcl_callback(pcl_msg):
     white_cloud = XYZRGB_to_XYZ(extracted_outliers)
     tree = white_cloud.make_kdtree()
 
+    # Create a cluster extraction object
     ec = white_cloud.make_EuclideanClusterExtraction()
-    ec.set_ClusterTolerance(0.001)
-    ec.set_MinClusterSize(10)
-    ec.set_MaxClusterSize(250)
+    # Set tolerances for distance threshold
+    # as well as minimum and maximum cluster size (in points)
+    # NOTE: These are poor choices of clustering parameters
+    # Your task is to experiment and find values that work for segmenting objects.
+    ec.set_ClusterTolerance(0.05)
+    ec.set_MinClusterSize(100)
+    ec.set_MaxClusterSize(1200)
+    # Search the k-d tree for clusters
     ec.set_SearchMethod(tree)
-
+    # Extract indices for each of the discovered clusters
     cluster_indices = ec.Extract()
 
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
 
+    rospy.loginfo("cluster_color: {}".format(len(cluster_color)))
+
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                             white_cloud[indice][1],
+                                             white_cloud[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+
+    # Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
 
     # TODO: Convert PCL data to ROS messages
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
     # TODO: Publish ROS messages
     pcl_objects_pub.publish(ros_cloud_objects)
     pcl_table_pub.publish(ros_cloud_table)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
     rospy.loginfo("pcl_objects and pcl_table published")
 
@@ -115,6 +139,7 @@ if __name__ == '__main__':
     # TODO: Create Publishers
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
 
     # Initialize color_list
     get_color_list.color_list = []
